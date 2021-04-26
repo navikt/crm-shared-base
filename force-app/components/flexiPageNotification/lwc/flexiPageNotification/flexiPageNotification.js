e457aa0db320accca5e43e31a5e7412d516b16c9
John Føreland <john.foreland@soprasteria.com>
2021-04-26 18:40:26 +0200

    use lwc for fetching fields, and better formatting

--- force-app/components/flexiPageNotification/lwc/flexiPageNotification/flexiPageNotification.js
+++ force-app/components/flexiPageNotification/lwc/flexiPageNotification/flexiPageNotification.js
@@ -1,36 +1,44 @@
 import { LightningElement, api, wire, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getFieldValue from '@salesforce/apex/FlexiPageNotificationController.getFieldValue';
+import { getRecord } from 'lightning/uiRecordApi';
 
 export default class FlexiPageNotification extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+    @track fields = [];
+
     @api title;
     @api field;
     @api variant;
     @api icon;
-    @api recordId;
+    @api titleOnTop;
 
     @track message;
     @track mainCss = 'slds-notify slds-notify_alert slds-theme_alert-texture round ';
-    @track titleCss = 'slds-text-title_caps slds-var-p-right_large ';
+    @track titleCss = 'slds-text-title_caps slds-var-m-left_x-small ';
 
     connectedCallback() {
         if (this.variant === 'error') {
-            this.mainCss = this.mainCss.concat('slds-theme_error');
+            this.mainCss += 'slds-theme_error';
             this.titleCss += 'slds-text-color_inverse';
         } else if (this.variant === 'info') {
-            this.mainCss = this.mainCss.concat('slds-theme_info');
+            this.mainCss += 'slds-theme_info';
             this.titleCss += 'slds-text-color_inverse';
         } else {
             // warning as fallback
-            this.mainCss = this.mainCss.concat('slds-theme_warning');
+            this.mainCss += 'slds-theme_warning';
+        }
+
+        this.fields = [this.objectApiName + '.' + this.field];
+    }
+
+    @wire(getRecord, { recordId: '$recordId', fields: '$fields' })
+    wiredRecord({ error, data }) {
+        if (data) {
+            this.message = data.fields[this.field].value;
         }
     }
 
-    @wire(getFieldValue, { recordId: '$recordId', field: '$field' })
-    deWire(result) {
-        if (result.data) {
-            this.message = result.data;
-            this.showNotification();
-        }
+    get titleSize() {
+        return this.titleOnTop ? 12 : undefined;
     }
 }
